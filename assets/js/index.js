d7c72d40310196bccad498a3bc2731d6f18ec3c6
Michael Langford <michaellangford99@gmail.com>
2023-07-25 20:16:13 -0600

    initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+//had to add: tags: 'ams', to the options
+
+//modified markdown-it-image-caption
+//return `<figure class="figure"><img src="${src}" class="figure-img img-fluid rounded" alt="${alt}" title="${title}"><figcaption class="figure-caption text-center" align="center">${title}</figcaption></figure>`
+    
